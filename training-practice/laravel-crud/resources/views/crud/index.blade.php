ecffa5624827a69b53f5cb8a861b721104e2d750
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-05-28 14:05:17 +0530

    add search box

--- training-practice/laravel-crud/resources/views/crud/index.blade.php
+++ training-practice/laravel-crud/resources/views/crud/index.blade.php
@@ -4,9 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <title>Student Details</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
 </head>
 <body>
     <div class="container my-5">
@@ -32,7 +31,7 @@
                                         <div class="row">
                                             <div class="col-lg-6 col-md-6 mb-3">
                                                 <label for="fname">Firstname</label>
-                                                <input type="text" class="form-control" id="fname" name="fname" placeholder="Jhon">
+                                                <input type="text" class="form-control" id="fname" name="fname" placeholder="John">
                                             </div>
                                             <div class="col-lg-6 col-md-6 mb-3">
                                                 <label for="lname">Lastname</label>
@@ -59,43 +58,78 @@
                 </div>
             </div>
         </div>
+        
+        <!-- Search input field -->
+        <div class="row mb-3">
+            <div class="col-lg-12">
+                <input type="text" id="searchInput" class="form-control" placeholder="Search for names..">
+            </div>
+        </div>
+
         <div class="row">
             <div class="col">
                 <table class="table table-bordered table-striped">
                     <thead>
                         <tr>
                             <th>No</th>
-                            <th>fullname</th>
+                            <th>Fullname</th>
                             <th>Email</th>
                             <th>Mobile</th>
                             <th>Action</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody id="studentTable">
                         @foreach($students as $student)
-                <tr>
-                    <td>{{ $student->id }}</td>
-                    <td>{{ $student->first_name }} {{ $student->last_name }}</td>
-                    <td>{{ $student->email }}</td>
-                    <td>{{ $student->mobile }}</td>
-                    <td>
-                        <a href="{{ route('students.edit', ['student' => $student->id]) }}">Edit</a> |
-                        
-                        <a href="#" onclick="event.preventDefault(); if (confirm('Are you sure you want to delete this student?')) { document.getElementById('delete-form-{{ $student->id }}').submit(); }">Delete</a>
-                    
-                        <form id="delete-form-{{ $student->id }}" action="{{ route('students.destroy', ['student' => $student->id]) }}" method="POST" style="display: none;">
-                            @csrf
-                            @method('DELETE')
-                        </form>
-                    </td>                    
-                </tr>
-            @endforeach
+                        <tr>
+                            <td>{{ $student->id }}</td>
+                            <td>{{ $student->first_name }} {{ $student->last_name }}</td>
+                            <td>{{ $student->email }}</td>
+                            <td>{{ $student->mobile }}</td>
+                            <td>
+                                <a href="{{ route('students.edit', ['student' => $student->id]) }}">Edit</a> |
+                                
+                                <a href="#" onclick="event.preventDefault(); if (confirm('Are you sure you want to delete this student?')) { document.getElementById('delete-form-{{ $student->id }}').submit(); }">Delete</a>
+                            
+                                <form id="delete-form-{{ $student->id }}" action="{{ route('students.destroy', ['student' => $student->id]) }}" method="POST" style="display: none;">
+                                    @csrf
+                                    @method('DELETE')
+                                </form>
+                            </td>                    
+                        </tr>
+                        @endforeach
                     </tbody>
-                    
                 </table>
             </div>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script>
+        document.getElementById('searchInput').addEventListener('keyup', function() {
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById('searchInput');
+            filter = input.value.toLowerCase();
+            table = document.getElementById('studentTable');
+            tr = table.getElementsByTagName('tr');
+
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName('td');
+                var found = false;
+                for (var j = 0; j < td.length; j++) {
+                    if (td[j]) {
+                        txtValue = td[j].textContent || td[j].innerText;
+                        if (txtValue.toLowerCase().indexOf(filter) > -1) {
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+                if (found) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        });
+    </script>
 </body>
 </html>
